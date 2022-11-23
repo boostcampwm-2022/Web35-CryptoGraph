a81b77f9bd5798a959dcbcda4ae892e47f380f23
sronger <kjt8709@nate.com>
2022-11-23 15:01:45 +0900

    refactor : treemap 구현 #18

--- client/pages/chart/treechart.tsx
+++ client/pages/chart/treechart.tsx
@@ -20,73 +20,70 @@ const margin = { top: 10, right: 10, bottom: 10, left: 300 },
   width = 2400 - margin.left - margin.right,
   height = 1000 - margin.top - margin.bottom
 
-const updateChart = (svgRef, data) => {
+const updateChart = (svgRef: React.RefObject<SVGSVGElement>, data) => {
   const chartArea = d3.select('svg#chart-area')
-  const test = data => {
-    const root = d3
-      .stratify()
-      .id(function (d) {
-        return d.name
-      }) // Name of the entity (column name is name in csv)
-      .parentId(function (d) {
-        return d.parent
-      })(
-      // Name of the parent (column name is parent in csv)
-      data
-    )
-    root.sum(function (d) {
-      return Math.abs(+d.value)
+  const root = d3
+    .stratify()
+    .id(function (d) {
+      return d.name
+    }) // Name of the entity (column name is name in csv)
+    .parentId(function (d) {
+      return d.parent
+    })(
+    // Name of the parent (column name is parent in csv)
+    data
+  )
+  root.sum(function (d) {
+    return Math.abs(+d.value)
+  })
+  d3.treemap().size([width, height]).padding(4)(root)
+  const [min, max] = [
+    d3.min(data, d => Math.abs(d.value)),
+    d3.max(data, d => d.value)
+  ]
+  let myScale = d3.scaleLinear().domain([min, max]).range([0.5, 1.5])
+  chartArea
+    .selectAll('rect')
+    .data(root.leaves())
+    .join('rect')
+    .attr('x', function (d) {
+      return d.x0
     })
-    d3.treemap().size([width, height]).padding(4)(root)
-    const [min, max] = [
-      d3.min(data, d => Math.abs(d.value)),
-      d3.max(data, d => d.value)
-    ]
-    let myScale = d3.scaleLinear().domain([min, max]).range([0.5, 1])
-    chartArea
-      .selectAll('rect')
-      .data(root.leaves())
-      .join('rect')
-      .attr('x', function (d) {
-        return d.x0
-      })
-      .attr('y', function (d) {
-        return d.y0
-      })
-      .attr('width', function (d) {
-        return d.x1 - d.x0
-      })
-      .attr('height', function (d) {
-        return d.y1 - d.y0
-      })
-      .attr('fill', function (d) {
-        return d.data.value > 0 ? 'red' : 'blue'
-      })
-      .transition()
-      .attr('opacity', function (d) {
-        return myScale(Math.abs(d.data.value))
-      })
-      .style('stroke', 'black')
-    chartArea
-      .selectAll('text')
-      .data(root.leaves())
-      .join('text')
-      .attr('x', function (d) {
-        return d.x0 + 30
-      }) // +10 to adjust position (more right)
-      .attr('y', function (d) {
-        return d.y0 + 200
-      }) // +20 to adjust position (lower)
-      .text(function (d) {
-        return d.data.name + String(Number(d.data.value).toFixed(2)) + '%'
-      })
-      .attr('font-size', '40px')
-      .attr('fill', 'white')
-  }
-  test(data)
+    .attr('y', function (d) {
+      return d.y0
+    })
+    .attr('width', function (d) {
+      return d.x1 - d.x0
+    })
+    .attr('height', function (d) {
+      return d.y1 - d.y0
+    })
+    .attr('fill', function (d) {
+      return d.data.value > 0 ? 'red' : 'blue'
+    })
+    .transition()
+    .attr('opacity', function (d) {
+      return myScale(Math.abs(d.data.value))
+    })
+    .style('stroke', 'black')
+  chartArea
+    .selectAll('text')
+    .data(root.leaves())
+    .join('text')
+    .attr('x', function (d) {
+      return d.x0 + Math.abs(d.x1 - d.x0) / 2 - 120 //d.x0 + 30
+    })
+    .attr('y', function (d) {
+      return d.y0 + Math.abs(d.y1 - d.y0) / 2
+    })
+    .text(function (d) {
+      return d.data.name + String(Number(d.data.value).toFixed(2)) + '%'
+    })
+    .attr('font-size', '40px')
+    .attr('fill', 'white')
 }
 
-const initChart = svgRef => {
+const initChart = (svgRef: React.RefObject<SVGSVGElement>) => {
   const chartContainer = d3.select(svgRef.current)
   chartContainer.attr('width', width)
   chartContainer.attr('height', height)
