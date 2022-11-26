706ba7074304df818d787d3020d6254dfe8b3aca
Kim Sanghun(BaldwinIV) <ksanghun10@gmail.com>
2022-11-26 23:37:34 +0900

    feat : 개발해야할 컴포넌트 파일 생성 #23

---
+++ client/components/CoinSelectController/index.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react'
+
+interface CoinSelectControllerProps {
+  value: number
+}
+
+//코인 상세정보
+export default function CoinSelectController(props: CoinSelectControllerProps) {
+  return <div />
+}
---
+++ client/components/RealTimeCoinPrice/index.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react'
+
+interface RealTimeCoinPriceProps {
+  value: number
+}
+
+//코인 상세정보
+export default function RealTimeCoinPrice(props: RealTimeCoinPriceProps) {
+  return <div />
+}
