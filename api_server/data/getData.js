91e83372a1546a7e25b34f865e499a3d94fca9fd
kong <70005144+kongyb@users.noreply.github.com>
2022-11-30 00:34:11 +0900

    feat: 백엔드 서버에서 사용할 data를 api이용하여 받아오는 함수 #47

---
+++ api_server/data/getData.js
@@ -0,0 +1,50 @@
+const axios = require("axios");
+require("dotenv").config();
+
+async function getUpbitMarketCode() {
+  const marketCodes = await axios({
+    method: "get",
+    baseURL: "https://api.upbit.com",
+    url: "/v1/market/all",
+  }).then((response) => response.data);
+  return marketCodes.reduce((acc, curr) => {
+    const [moneyType, coinCode] = curr.market.split("-");
+    if (moneyType === "KRW") {
+      acc.push(coinCode);
+    }
+    return acc;
+  }, []);
+}
+
+async function getCoinData() {
+  const responseBody = await axios({
+    method: "get",
+    baseURL: "https://pro-api.coinmarketcap.com",
+    url: "v1/cryptocurrency/listings/latest?start=1&limit=5000&convert=KRW",
+    headers: {
+      Accept: "application/json",
+      "X-CMC_PRO_API_KEY": process.env.COINMARKETCAP_API_KEY,
+    },
+  }).then((response) => response.data);
+  return responseBody.data;
+}
+
+/**
+ *
+ * @param {string} coinIds 조회할 코인ID들을 ,로 연결한 문자열
+ * @returns
+ */
+async function getCoinMetaData(coinIds) {
+  const responseBody = await axios({
+    method: "get",
+    baseURL: "https://pro-api.coinmarketcap.com",
+    url: `/v2/cryptocurrency/info?id=${coinIds}`,
+    headers: {
+      Accept: "application/json",
+      "X-CMC_PRO_API_KEY": process.env.COINMARKETCAP_API_KEY,
+    },
+  }).then((response) => response.data);
+  return responseBody.data;
+}
+
+module.exports = { getUpbitMarketCode, getCoinData, getCoinMetaData };
