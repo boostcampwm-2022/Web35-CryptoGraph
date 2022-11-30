e0261ebb9f68e63572e9045cb90fee2912928ad4
sronger <kjt8709@nate.com>
2022-11-30 16:21:55 +0900

    feat : 코인 선택 레이아웃 설정 #61

--- client/components/CoinSelectController/index.tsx
+++ client/components/CoinSelectController/index.tsx
@@ -1,10 +1,64 @@
 import * as React from 'react'
+import { styled } from '@mui/material/styles'
+import Checkbox from '@mui/material/Checkbox'
 
 interface CoinSelectControllerProps {
-  value: number
+  isSideBarOpened: boolean
 }
+const SelectCoinTitle = styled('div')`
+  display: flex;
+  justify-content: space-between;
+  margin: 24px;
+  align-items: center;
+`
+const SelectCoinTitleFont = styled('div')`
+  font-size: 24px;
+`
+
+const SelectCoinTitleBox = styled('div')`
+  display: flex;
+  align-items: center;
+`
+
+const SelectCoinTitleBoxFont = styled('div')`
+  font-size: 16px;
+`
+
+const SelectCoinInnerLayer = styled('div')`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin: 16px 24px;
+`
+
+const SelectCoinInnerFont = styled('div')`
+  align-items: center;
+  margin: 16px;
+  font-size: 24px;
+`
 
 //코인 상세정보
 export default function CoinSelectController(props: CoinSelectControllerProps) {
-  return <div />
+  const coinList = ['bit', 'eth', 'xrp', 'doge']
+  return (
+    <>
+      <div style={{ display: props.isSideBarOpened ? 'block' : 'none' }}>
+        <SelectCoinTitle>
+          <SelectCoinTitleFont>코인선택</SelectCoinTitleFont>
+          <SelectCoinTitleBox>
+            <SelectCoinTitleBoxFont>전부 [선택/해제]</SelectCoinTitleBoxFont>
+            <Checkbox />
+          </SelectCoinTitleBox>
+        </SelectCoinTitle>
+        {coinList.map((coin, index) => {
+          return (
+            <SelectCoinInnerLayer key={index}>
+              <SelectCoinInnerFont>{coin}</SelectCoinInnerFont>
+              <Checkbox />
+            </SelectCoinInnerLayer>
+          )
+        })}
+      </div>
+    </>
+  )
 }
