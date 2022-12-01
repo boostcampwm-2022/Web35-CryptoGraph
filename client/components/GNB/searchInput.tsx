054439fbce742db29a2bc3ed25940318c0daa9b2
rkdwoals159 <rkdwoals159@gmail.com>
2022-12-01 16:01:17 +0900

    feat : cors해결 이후 자동완성 data fetch 및 기능 구현완료 + 타입 지정완료 / 추후 리다이렉트 기능 구현 #68

--- client/components/GNB/searchInput.tsx
+++ client/components/GNB/searchInput.tsx
@@ -4,51 +4,59 @@ import Stack from '@mui/material/Stack'
 import Autocomplete from '@mui/material/Autocomplete'
 import SearchIcon from '@mui/icons-material/Search'
 import HighlightOffIcon from '@mui/icons-material/HighlightOff'
+import { getMarketCapInfo } from '@/utils/metaDataManages'
+import { MarketCapInfo } from '@/types/CoinDataTypes'
 export default function SearchInput() {
   /* -------------주의------------- */
   // CoinNames의 fetch는 api_server를 이용했습니다.
   // express서버를 키고 작동해야 제대로 받아올 수 있습니다.
-  fetchCoinNames().then(CoinNames => {
-    return (
-      <Stack spacing={2} sx={{ width: 300 }}>
-        <Autocomplete
-          freeSolo
-          id="free-solo-2-demo"
-          disableClearable
-          options={CoinNames.map((coin: any) => coin.name)}
-          renderInput={params => (
-            <TextField
-              sx={{ marginLeft: 'auto' }}
-              {...params}
-              InputProps={{
-                ...params.InputProps,
-                type: 'search',
-                sx: {
-                  backgroundColor: 'white',
-                  height: '48px',
-                  width: { mobile: '100%', tablet: 400, desktop: 600 },
-                  p: 2,
-                  gap: 2
-                },
-                placeholder: '검색어를 입력하세요',
-                startAdornment: <SearchIcon sx={{ opacity: 0.2 }} />,
-                endAdornment: <HighlightOffIcon sx={{ opacity: 0.2 }} />
-              }}
-            />
-          )}
-        />
-      </Stack>
-    )
-  }) //next.js v13
-}
-
-//원래 fetch부분을 SSR로 미리 받아올까했었는데
-//Next.js 13버전에서는 더이상 getServerSideProps를 사용하지 않는다고해서
-//그냥 이렇게 SSG로 만들어 봤습니다. 어차피 저희는 코인 이름만 필요하니까요
-//참고자료 https://youtu.be/5BRFGMs1v_o?t=137
-//참고자료2 https://itchallenger.tistory.com/778
-export async function fetchCoinNames() {
-  const res = await fetch('http://localhost:8080/market-cap-info')
-  const data = await res.json()
-  return data
+  const [CoinNames, setCoinNames] = React.useState<MarketCapInfo[]>([])
+  React.useEffect(() => {
+    async function asyncGetCoinName() {
+      const data: MarketCapInfo[] | null = await getMarketCapInfo()
+      if (!data) {
+        console.error('검색바 자동완성기능 데이터 fetch 에러')
+        return
+      }
+      setCoinNames(data)
+    }
+    asyncGetCoinName()
+  }, [])
+  return (
+    <Stack spacing={2} sx={{ width: 300 }}>
+      <Autocomplete
+        freeSolo
+        id="free-solo-2-demo"
+        disableClearable
+        options={
+          CoinNames.length
+            ? [
+                ...CoinNames.map((coin: MarketCapInfo) => coin.name),
+                ...CoinNames.map((coin: MarketCapInfo) => coin.name_kr)
+              ]
+            : [] //CoinName 검증을 위한 삼항연산자 만일 fetch 되지않았으면 껍데기만 보이게 함
+        }
+        renderInput={params => (
+          <TextField
+            sx={{ marginLeft: 'auto' }}
+            {...params}
+            InputProps={{
+              ...params.InputProps,
+              type: 'search',
+              sx: {
+                backgroundColor: 'white',
+                height: '48px',
+                width: { mobile: '100%', tablet: 400, desktop: 600 },
+                p: 2,
+                gap: 2
+              },
+              placeholder: '검색어를 입력하세요',
+              startAdornment: <SearchIcon sx={{ opacity: 0.2 }} />,
+              endAdornment: <HighlightOffIcon sx={{ opacity: 0.2 }} />
+            }}
+          />
+        )}
+      />
+    </Stack>
+  )
 }
