640d252b221c77cd1ddf5000ddaeb5584d424ddb
sronger <kjt8709@nate.com>
2022-12-01 16:27:56 +0900

    feat : 코인 선택 데이터 바인딩 #61

--- client/components/CoinSelectController/index.tsx
+++ client/components/CoinSelectController/index.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react'
 import { styled } from '@mui/material/styles'
 import Checkbox from '@mui/material/Checkbox'
+import { useEffect, useState } from 'react'
+import Image from 'next/image'
 
 interface CoinSelectControllerProps {
   isSideBarOpened: boolean
@@ -8,7 +10,7 @@ interface CoinSelectControllerProps {
 const SelectCoinTitle = styled('div')`
   display: flex;
   justify-content: space-between;
-  margin: 24px;
+  margin: 8% 12%;
   align-items: center;
 `
 const SelectCoinTitleFont = styled('div')`
@@ -21,28 +23,63 @@ const SelectCoinTitleBox = styled('div')`
 `
 
 const SelectCoinTitleBoxFont = styled('div')`
-  font-size: 16px;
+  font-size: 12px;
 `
 
 const SelectCoinInnerLayer = styled('div')`
   display: flex;
   justify-content: space-between;
   align-items: center;
-  margin: 16px 24px;
+  margin: 8% 12%;
 `
 
 const SelectCoinInnerFont = styled('div')`
   align-items: center;
-  margin: 16px;
+  margin: 8% 12%;
   font-size: 24px;
 `
 
+const SelectCoinHead = styled('div')`
+  overflow: scroll;
+  height: 100%;
+`
+
+const getCoinInfo = async () => {
+  const res = await fetch(
+    process.env.NEXT_PUBLIC_SERVER_URL + '/market-cap-info',
+    {
+      method: 'GET',
+      headers: { accept: 'application/json' }
+    }
+  )
+  return res.json()
+}
+
+const getData = () => {
+  return 'hello'
+}
+
 //코인 상세정보
 export default function CoinSelectController(props: CoinSelectControllerProps) {
-  const coinList = ['bit', 'eth', 'xrp', 'doge']
+  const [coinList, setCoinList] = useState([])
+  // {
+  //   name: 영문이름,
+  //   name_kr: 한글이름,
+  //   logo: 이미지 url,
+  //   cmc_rank: 시가총액 순위
+  //   }[ ]
+
+  useEffect(() => {
+    getCoinInfo().then(data => {
+      setCoinList(data)
+    })
+  }, [])
+
   return (
     <>
-      <div style={{ display: props.isSideBarOpened ? 'block' : 'none' }}>
+      <SelectCoinHead
+        style={{ display: props.isSideBarOpened ? 'block' : 'none' }}
+      >
         <SelectCoinTitle>
           <SelectCoinTitleFont>코인선택</SelectCoinTitleFont>
           <SelectCoinTitleBox>
@@ -53,12 +90,13 @@ export default function CoinSelectController(props: CoinSelectControllerProps) {
         {coinList.map((coin, index) => {
           return (
             <SelectCoinInnerLayer key={index}>
-              <SelectCoinInnerFont>{coin}</SelectCoinInnerFont>
-              <Checkbox />
+              <Image src={coin.logo} alt="" width={44} height={44} />
+              <SelectCoinInnerFont>{coin.name_kr}</SelectCoinInnerFont>
+              <Checkbox onClick={getData} />
             </SelectCoinInnerLayer>
           )
         })}
-      </div>
+      </SelectCoinHead>
     </>
   )
 }
