ba1d819b5fdf8cdd79552493e58755b6e5f59f0f
kong <70005144+kongyb@users.noreply.github.com>
2022-12-06 14:08:02 +0900

    fix: 빌드실패 야기하는 디렉토리 삭제

--- client/pages/chart/runningchart/index.tsx
+++
@@ -1,9 +0,0 @@
-import { RunningChart } from '@/components/RunningChart'
-
-export default function RunningChartPage() {
-  return (
-    <>
-      <RunningChart candleCount={20}></RunningChart>
-    </>
-  )
-}
