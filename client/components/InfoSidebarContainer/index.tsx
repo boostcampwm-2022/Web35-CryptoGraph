af4cf06a2af4ae14b2ff2e41686a3718293b390e
kong <70005144+kongyb@users.noreply.github.com>
2022-12-07 16:51:34 +0900

    refactor: 사용되지않는 컴포넌트 제거 #103

--- client/components/InfoSidebarContainer/index.tsx
+++ client/components/InfoSidebarContainer/index.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react'
-import { styled } from '@mui/material'
 import LinkButton from '@/components/LinkButton'
 import { ReactNode } from 'react'
 
@@ -13,19 +12,8 @@ export default function InfoSidebarContainer({
     <>
       {React.Children.map(children, child => {
         return child
-        // return <StyledRTV>{child}</StyledRTV>
       })}
       <LinkButton goto="/" content="Go to Main" />
     </>
   )
 }
-
-const StyledRTV = styled('div')`
-  display: flex;
-  width: 100%;
-  height: 100%;
-  background-color: #ffffff;
-  /* border: 1px solid #cac4d0;
-  border-radius: 20px; */
-  padding-bottom: 8px;
-`
