fe91e8f26c7085351354b816776022e1d2a5fdfe
Kim Sanghun(BaldwinIV) <ksanghun10@gmail.com>
2022-12-09 00:23:32 +0900

    style : 차트 선택 border 제거 #118

--- client/components/ChartSelectController/index.tsx
+++ client/components/ChartSelectController/index.tsx
@@ -18,8 +18,6 @@ const ChartPeriodSelectorContainer = styled('div')`
   height: 10%;
   background-color: #ffffff;
   box-sizing: border-box;
-  border: 1px solid #cac4d0;
-  border-radius: 20px;
   justify-content: center;
   align-items: center;
   margin-bottom: 20px;
